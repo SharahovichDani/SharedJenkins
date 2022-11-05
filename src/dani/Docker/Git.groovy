19470ce84e0d41bfe610c7dd2e301367e489d113
SharahovichDani <94566235+SharahovichDani@users.noreply.github.com>
2022-11-05 17:33:27 +0200

    Update Git.groovy

--- src/dani/Docker/Git.groovy
+++ src/dani/Docker/Git.groovy
@@ -13,7 +13,7 @@ class Git implements Serializable {
             pack.sh '''git remote set-url origin https://'''+pack.PASS+'''@github.com/SharahovichDani/test.git'''
             pack.sh 'git add .'
             pack.sh 'git commit -m "[ci skip]"'
-            pack.sh 'git push origin HEAD:main'
+            pack.sh 'git push -u origin main'
         }
     }
     def Config(String Name, String Mail) {
